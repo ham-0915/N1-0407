e6396b9449508484554bbc539197e33d2727d43a
ham-0915 <ham-0915@yandex.com>
2026-05-21 06:38:22 +0800

    更新 diy-part2.sh

--- N1/Immortalwrt/24.10/diy-part2.sh
+++ N1/Immortalwrt/24.10/diy-part2.sh
@@ -48,4 +48,4 @@ git clone https://github.com/timsaya/luci-app-bandix --depth=1 package/luci-app-
 git clone https://github.com/timsaya/openwrt-bandix --depth=1 package/openwrt-bandix
 
 # git clone https://github.com/sirpdboy/luci-app-timecontrol --depth=1 package/luci-app-timecontrol
-# git clone https://github.com/sbwml/luci-app-quickfile --depth=1 package/luci-app-quickfile
+git clone https://github.com/sbwml/luci-app-quickfile --depth=1 package/luci-app-quickfile
